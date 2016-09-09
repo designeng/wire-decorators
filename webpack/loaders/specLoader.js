6c3b48b5872d9356deea823726652f0a66dab002
designeng <denissavenok@gmail.com>
2016-09-09 13:00:54 +0300

    specLoader

--- webpack/loaders/specLoader.js
+++ webpack/loaders/specLoader.js
@@ -1,28 +1,55 @@
 var esprima = require('esprima');
 const CLIENT = 'client';
 const SERVER = 'server';
-// const SERVER_REGEX = /@server((\s)|(\([\S\s]+\)))?/g;
-const SERVER_REGEX = /@server(\s)/g;
 
-var program = `
-    var a = {
-    serverComponent: {
-        test : {
-    },
-}`;
+// TODO: @server(...) match
+// const SERVER_DECORATOR_REGEX = /@server((\s)|(\([\S\s]+\)))?/g;
+// 
+const SERVER_DECORATOR_REGEX = /@server(\s)/g;
+const CLOSING_BRACKET_REGEX = /\}/g;
+
+const getComponentName = (str) => {
+    var colonPosition = str.indexOf(':');
+    return str.slice(0, colonPosition).trim();
+}
 
 module.exports = function specLoader(source) {
     var env = process.env.ENVIRONMENT;
 
     if(env === CLIENT) {
+        var serverDecoratorMatches, 
+            closingBracketMatches, 
+            componentFragment, 
+            componentSource,
+            serverDecoratorPosition,
+            lastBracketIncludedPosition,
+            subSource,
+            offset,
+            program;
 
-    }
+        while(serverDecoratorMatches = SERVER_DECORATOR_REGEX.exec(source)) {
+            offset = serverDecoratorMatches[0].length;
+            // add `@server ` length to remove all server decorators from sub source string
+            serverDecoratorPosition = serverDecoratorMatches.index + offset;
+            subSource = source.slice(serverDecoratorPosition);
 
-    try {
-        var ast = esprima.parse(program)
-        console.log('AST:::', ast);
-    } catch (e) {
-        console.log('ERROR:::', e);
+            while(closingBracketMatches = CLOSING_BRACKET_REGEX.exec(subSource)) {
+                var lastBracketIncludedPosition = closingBracketMatches.index + 1;
+                componentFragment = subSource.slice(0, lastBracketIncludedPosition);
+
+                program = `var a = {${componentFragment}}`;
+                
+                try {
+                    var ast = esprima.parse(program);
+                    var componentName = getComponentName(componentFragment);
+                    source = source.replace(componentFragment, `${componentName}: null`);
+                } catch (e) {
+                    continue;
+                }
+            }
+        }
     }
+    
+    source = source.replace(SERVER_DECORATOR_REGEX, '');
     return source;
 };
