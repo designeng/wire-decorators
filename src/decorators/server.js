73510c6e3c09c412e5ee9fb92794b827958dea67
designeng <denissavenok@gmail.com>
2016-09-09 14:19:58 +0300

    cleaned up code

--- src/decorators/server.js
+++ src/decorators/server.js
@@ -1,9 +1,7 @@
-import _ from 'underscore';
-
 let env = process.env.ENVIRONMENT;
 
 export default function server(target, key, descriptor) {
     if (env === 'server') {
-        
+        // nothing interesting here
     }
 }
