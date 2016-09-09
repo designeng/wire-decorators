093a6bf29dd3b7c18608d8ec2215011296c016b4
designeng <denissavenok@gmail.com>
2016-09-09 13:26:39 +0300

    comment

--- src/decorators/client.js
+++ src/decorators/client.js
@@ -3,13 +3,10 @@ let env = process.env.ENVIRONMENT;
 
 export default function client(target, key, descriptor) {
     if (env === 'server') {
-        // TODO: is not it dangerous? Client components will be removed.
-
         delete target[key];
         return target = _.omit(target, key);
 
-        // maybe:
+        // TODO: Client components will be removed by previos code, but maybe better:
         // target[key] = null;
-        // END TODO
     }
 }
